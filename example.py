8e835554bdc2e8ba74ea83b7c89f56a3ef9d0600
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-25 17:05:10 +0900

    dump

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ d['id'] = 1
 d['name'] = 'pie'
 d['hobby'] = ['game', 'tv', 'work']
 d['test'] = True
-file_path = 'C:\Users\82103\OneDrive - 인하대학교\바탕 화면\json'
-with open(file_path, 'w', encoding='UTF-8') as fp:
-    json.dump(d, fp)
+file_path='C:\pydata\json\dictd.json'
+with open(file_path, 'w',encoding='UTF-8') as fp:
+    json.dump(d,fp,indent=4)
+
