f6c58e274aabcb444317f20622add1106956e6e2
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-26 13:27:16 +0900

    file control

---
+++ hw.py
@@ -0,0 +1,9 @@
+# import os
+# path='C:/Users/user/Desktop/data/calib'
+# file=os.listdir(path)
+# print(file)
+# print(type(file))
+dict=['a','b','c','d','e']
+for i in dict:
+    print(i)
+    print(dict)
