dea84fd8f6fad529b8910720609cc306f1ed0771
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-27 16:47:34 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,44 @@
-x=['a','b','c']
-print(x)
-dict_a = dict()
-dict_a[x[0]]=[1,2,3]
-x[0] = dict_a
-print(x)
+# print('i eat %s' %"aa") #format
+# print('i eat {0} and {1}'.format('aaa','bbb'))
+# name='ccc'
+# age='ddd'
+# print(f'i eat {name} and {age}')
+# matrix1=[[1,2,3],[4,5,6],[7,8,9]]
+# matrix2=[[1,2,3],[4,5,6],[7,8,9]]
+# a = {'name': 'pey', 'phone': '010-9999-1234', 'birth': '1118'}
+# print(a.values())
+# print('name' in a)
+# s1=set([1,2,3,4,5,6])
+# s2=set([4,5,6,7,8,9])
+# print(s1|s2)
+# b=a[:] #b=copy(a) 와 동치
+# num=0
+# prompt="1. Add\n2. Del\n3. List\n4. Quit"
+# while num!=4:
+#     print(prompt)
+#     num=int(input())
+# i=0
+# while True:
+#     print("Crtl+C %d" %i)
+#     i=i+1
+for i in range(2,10):
+    for j in range(1,10):
+        print(i*j,end=" ")
+    print("") #print는 디폴트값으로 줄바꿈이 세팅되어있다.
+a=[1,2,3,4]
+result=[num*3 for num in a] #리스트 컴프리헨션
+
+def add_many(*args):
+    result =0
+    for i in args:
+        result=result+i
+    return result
+print(add_many(1,2,3,4,5,6,7))
+
+def print_kword(**kv):
+    print(kv)
+print_kword(a=1,b=2,c='a')
+
+f='C:\\pydata\\json\\name.txt'
+
+print(f.replace("\\","/"))
